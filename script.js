0712ce7643d8eff5d8374e010b74b1ed4eb7f0e3
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-15 13:45:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-//your JS code here. If required.
+const btn = document.getElementById("btn");
+const input = document.getElementById("ip");
+const output = document.getElementById("output");
+
+function delay(time, value) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(value), time);
+  });
+}
+
+btn.addEventListener("click", () => {
+
+  let number = Number(input.value);
+
+  delay(2000, number)
+
+    .then(num => {
+      output.textContent = `Result: ${num}`;
+      return delay(2000, num * 2);
+    })
+
+    .then(num => {
+      output.textContent = `Result: ${num}`;
+      return delay(1000, num - 3);
+    })
+
+    .then(num => {
+      output.textContent = `Result: ${num}`;
+      return delay(1000, num / 2);
+    })
+
+    .then(num => {
+      const final = num + 10;
+      output.textContent = `Final Result: ${final}`;
+    });
+
+});
